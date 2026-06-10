67cccac757e1a67a212e84742ad7ce297389f9f0
homasapienss <homa19222@gmail.com>
2026-06-10 17:06:14 +0300

    отрефакторил сущности для spring jpa

--- src/main/java/edu/currency/exchange/homasapienss/entities/Currency.java
+++ src/main/java/edu/currency/exchange/homasapienss/entities/Currency.java
@@ -1,49 +1,20 @@
 package edu.currency.exchange.homasapienss.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "currencies")
+@Entity
 public class Currency {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String code;
     private String name;
     private String sign;
-
-    public Currency(String code, String name, String sign) {
-        this.code = code;
-        this.name = name;
-        this.sign = sign;
-    }
-
-    public Currency() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSign() {
-        return sign;
-    }
-
-    public void setSign(String sign) {
-        this.sign = sign;
-    }
 }
--- src/main/java/edu/currency/exchange/homasapienss/entities/ExchangeRate.java
+++ src/main/java/edu/currency/exchange/homasapienss/entities/ExchangeRate.java
@@ -1,51 +1,26 @@
 package edu.currency.exchange.homasapienss.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.math.BigDecimal;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "exchange_rates")
+@Entity
 public class ExchangeRate {
-    private int id;
-    private int baseCurrency;
-    private int targetCurrency;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "base_currency_id", nullable = false)
+    private Currency baseCurrency;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "target_currency_id", nullable = false)
+    private Currency targetCurrency;
     private BigDecimal rate;
-
-    public ExchangeRate(int baseCurrency, int targetCurrency, BigDecimal rate) {
-        this.baseCurrency = baseCurrency;
-        this.targetCurrency = targetCurrency;
-        this.rate = rate;
-    }
-
-    public ExchangeRate() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getBaseCurrency() {
-        return baseCurrency;
-    }
-
-    public void setBaseCurrency(int baseCurrency) {
-        this.baseCurrency = baseCurrency;
-    }
-
-    public int getTargetCurrency() {
-        return targetCurrency;
-    }
-
-    public void setTargetCurrency(int targetCurrency) {
-        this.targetCurrency = targetCurrency;
-    }
-
-    public BigDecimal getRate() {
-        return rate;
-    }
-
-    public void setRate(BigDecimal rate) {
-        this.rate = rate;
-    }
 }
